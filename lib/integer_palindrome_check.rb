96ac6ddf529a126607a2bcd941c3b500c9b0e104
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-03-31 15:01:34 -0500

    All tests passing

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,25 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  return false unless number.is_a? Integer
+  return false if number < 0
+  return true if number < 10
+
+  n = find_digits(number)
+  i = n / 2
+
+  i.times do |i|
+    last = number / 10**i % 10
+    first = number / 10**(n-i-1) % 10
+    return false if first != last
+  end
+  return true
+end
+
+def find_digits(num)
+  x = 1
+  while num >= 10
+    num /= 10
+    x += 1
+  end
+  return x
 end
